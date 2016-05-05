c143006ca51894eacddb41d969fea2159aacb776
Akash Trehan <akash.trehan123@gmail.com>
2016-05-06 01:00:04 +0530

    Added some static files and urlpatterns

--- hacker_news/urls.py
+++ hacker_news/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
+from hacker_news import views
 from hacker_news.views import NewsListView
 
 urlpatterns = [
     url(r'^$', NewsListView.as_view(), name='news_list'),
+    url(r'^register', views.register, name='register'),
+    url(r'^login', views.login, name='login'),
 ]
--- hacker_news/views.py
+++ hacker_news/views.py
@@ -8,3 +8,10 @@ class NewsListView(ListView):
     queryset = News.objects.order_by("-date")[:10]
     template_name = 'hacker-news/news.html'
     context_object_name = 'news'
+
+def register(request):
+    return render(request, 'hacker-news/register.html')
+
+def login(request):
+    return render(request, 'hacker-news/login.html')
+
