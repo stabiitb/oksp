ef8c0bbbf8e8e4fd7a83b075a825d01d4d2af03a
Arunabh98 <arunabhghosh98@gmail.com>
2016-05-06 10:51:28 +0530

    News.html is able to display uploaded links

--- hacker_news/views.py
+++ hacker_news/views.py
@@ -2,6 +2,8 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 from hacker_news.models import New
 
@@ -9,9 +11,16 @@ from .forms import NewsUploadForm
 
 
 class NewsListView(ListView):
-    queryset = New.objects.order_by("-date")[:10]
+    queryset = New.objects.all()#order_by("-date")[:10]
     template_name = 'hacker-news/news.html'
     context_object_name = 'news'
+
+    def get(self, request, *args, **kwargs):
+        context = locals()
+        context[self.context_object_name] = self.queryset
+        return render_to_response(self.template_name, context, context_instance=RequestContext(request))
+
+
 
 def register(request):
     return render(request, 'hacker-news/register.html')
